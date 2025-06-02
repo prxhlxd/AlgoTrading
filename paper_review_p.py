dcd6deb888dff5fd7ad3f1c3b47c1847faf1e0c6
prxhlxd <22f3000813@ds.study.iitm.ac.in>
2025-06-02 15:52:17 +0530

    Initial commit

--- paper_review_p.py
+++ paper_review_p.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def show_paper_dashboard():
     # Page configuration
